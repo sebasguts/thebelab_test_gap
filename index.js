48e5f8d3bad79cf395ae1e5569f751c6278245ca
Sebastian Gutsche <gutsche@momo.math.rwth-aachen.de>
2017-10-20 11:53:37 +0200

    Update with status indicator

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-function localKernel() {
-  // request a kernel from a local notebook serve4r
-  // assumes notebook was started with:
-  //   jupyter notebook --NotebookApp.allow_origin=* --NotebookApp.token=test-secret
-  return thebelab.RequestKernel({
-    serverSettings: {
-      baseUrl: "http://127.0.0.1:8888/",
-      token: "test-secret",
-    },
-    name: "python3",
-  });
-}
-
-function binderKernel() {
-  // request a kernel from Binder
-  return thebelab
-    .requestBinder({
-      repo: "sebasguts/GAPBinderDemo",
-    })
-    .then(serverSettings => {
-      console.log("binder settings", serverSettings);
-      return thebelab.requestKernel({
-        serverSettings,
-        name: "gap-native",
-      });
-    });
-}
-
-window.onload = function() {
-  let cells = thebelab.renderAllCells();
-  binderKernel().then(kernel => {
-    thebelab.hookupKernel(kernel, cells);
-  });
-};
+// function localKernel() {
+//   // request a kernel from a local notebook serve4r
+//   // assumes notebook was started with:
+//   //   jupyter notebook --NotebookApp.allow_origin=* --NotebookApp.token=test-secret
+//   return thebelab.RequestKernel({
+//     serverSettings: {
+//       baseUrl: "http://127.0.0.1:8888/",
+//       token: "test-secret",
+//     },
+//     name: "python3",
+//   });
+// }
+// 
+// function binderKernel() {
+//   // request a kernel from Binder
+//   return thebelab
+//     .requestBinder({
+//       repo: "sebasguts/GAPBinderDemo",
+//     })
+//     .then(serverSettings => {
+//       console.log("binder settings", serverSettings);
+//       return thebelab.requestKernel({
+//         serverSettings,
+//         name: "gap-native",
+//       });
+//     });
+// }
+// 
+// window.onload = function() {
+//   let cells = thebelab.renderAllCells();
+//   binderKernel().then(kernel => {
+//     thebelab.hookupKernel(kernel, cells);
+//   });
+// };
